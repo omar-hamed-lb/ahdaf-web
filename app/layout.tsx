5d9ab9098ec1bf14c7cc99f5c743ab83c66d7065
Omar Hamed <omar_hamed@hotelmap.com>
2024-12-28 13:55:49 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-X6HY8D5F8T" />
     </html>
   );
 }
