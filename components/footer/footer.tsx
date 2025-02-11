e28c55745b3ac3db901724ee91d46f85e843e865
Omar Hamed <omar_hamed@hotelmap.com>
2025-02-11 15:53:18 +0200

    update

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="py-6 text-center bg-[#0b1c39] text-white">
-      <p>&copy; 2025 Ahdaf Charity. All rights reserved.</p>
+      <p>
+        {" "}
+        وقف أهداف الخيري <br></br>&copy; 2025 Ahdaf Charity. All rights
+        reserved.
+      </p>
     </footer>
   );
 }
