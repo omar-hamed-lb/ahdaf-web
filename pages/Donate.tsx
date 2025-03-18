bd382d082d69db0b743dca2a652b947f9dc02629
AlaaMallat <140802536+AlaaMallat@users.noreply.github.com>
2025-03-18 11:54:32 +0200

    update

--- pages/Donate.tsx
+++ pages/Donate.tsx
@@ -75,7 +75,7 @@ export default function Donate() {
       {/* Videos */}
       <section className="min-h-screen flex items-center justify-center">
         <div className="w-full max-w-4xl p-6">
-          <h1 className="text-4xl font-bold text-center text-cgreen mb-2 mt-6">Ramadan 2025 Campaign</h1>
+          <h1 className="text-3xl font-bold text-center text-cgreen mb-2 mt-6">Ramadan 2025 Campaign</h1>
           <h1 className="text-3xl font-bold text-center text-cgreen mb-6">#ATA’UKA_ALLAMANI</h1>
           <div className="relative overflow-hidden rounded-lg aspect-video">
             <iframe
